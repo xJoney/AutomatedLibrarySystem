6de07f059f64852fdf964539c1372f85e16bd800
Jonathan <jonathanhua@hotmail.com>
2025-11-18 03:09:34 -0500

    fix: updated redis connection

--- server/redis.ts
+++ server/redis.ts
@@ -1,6 +1,10 @@
 import { createClient } from "redis";
 
 // sets up the redis access that will listen on port 6379
-export const redis = createClient({ url: "redis://localhost:6379" });
+// export const redis = createClient({ url: "redis://localhost:6379" });
+export const redis = createClient({
+  url: process.env.REDIS_URL || "redis://redis:6379"
+});
+
 await redis.connect();
 
