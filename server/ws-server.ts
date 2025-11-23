e42cc242334b8f32f435d65a40d41723add450ea
jonathanhua <jonathanhua@hotmail.com>
2025-11-23 16:59:23 -0500

    fix: subscriber now global instead of creating sub per web socket connection

--- server/ws-server.ts
+++ server/ws-server.ts
@@ -1,24 +1,33 @@
-import { WebSocketServer } from 'ws'
+import { WebSocketServer, WebSocket } from 'ws'
 import { redis } from './redis.js'
 
+const clients = new Set<WebSocket>()
+console.log("Websocket server running on ws://localhost:4000");
+
+
+const sub = redis.duplicate()
+await sub.connect()
+
+// forward popularity updates to the client
+await sub.subscribe('popularity', (message) => {
+  console.log("Redis message received:", message)
+
+  for (const client of clients) {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(message)
+    }
+  }
+})
 
 //standalone websocket using ws library, acts like bridge between Redis and frontend
-
-const wss = new WebSocketServer({ port: 8081 }) 
+const wss = new WebSocketServer({ port: 4000 }) 
 wss.on('connection', async (ws) => {
   console.log('websocked client connected')
-
-  const sub = redis.duplicate()
-  await sub.connect()
-
-  // forward popularity updates to the client
-  await sub.subscribe('popularity', (message) => ws.send(message))
+  clients.add(ws)
 
   ws.on('close', async () => {
-    await sub.unsubscribe('popularity')
-    await sub.quit()
-    console.log('websocket is closed')
+    clients.delete(ws)
+    console.log('websocket client disconnected')
   })
 })
 
-console.log('WebSocket server running on ws://localhost:8081')
